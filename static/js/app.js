1b80a43592cb84a1393b83a53cf4c07f5946911c
Pushkal Katara <pushkalkatara@pushkals-air.wifi.local.cmu.edu>
2023-09-28 17:12:28 -0400

    update

--- static/js/app.js
+++ static/js/app.js
@@ -27,61 +27,15 @@ $(document).ready(function() {
          }, 'text');
     });
 
-    // var current_cmd_idxs = {
-    //     "faucet": 1
-    // }
-
     var current_domain = "faucet"
 
-    var vid_start_times = {
-        "faucet": 0
-    }
-
-    var vid_end_times = {
-        "faucet": 5
-    }
-
-    function playSeg(vid, start_time, end_time, domain_name) {
-        vid.play();
-        vid.pause();
-        vid.currentTime = start_time;
-        vid.play();
-
-        // console.log("start and end: " + start_time.toString() + ", " + end_time.toString());
-
-        var pausing_function = function() {
-            // console.log("checking pausing function cb for " + domain_name);
-            // console.log("current and end time");
-            // console.log(this.currentTime);
-            // console.log(end_time)
-            if (this.currentTime >= end_time) {
-                // console.log("reached end time");
-                this.pause();
-                this.removeEventListener("timeupdate", pausing_function);
-            }
-        };
-
-        // console.log("adding timeupdate pausing_function for " + domain_name + "_" + desired_cmd_idx.toString());
-        vid.addEventListener("timeupdate", pausing_function);
-    }
-
     $('select').on('change', function() {
-        // var sep_idx = this.value.indexOf('_'); 
-        // var domain_name = this.value.substring(0, sep_idx);
-        // var desired_cmd_idx = parseInt(this.value.substring(sep_idx + 1)); 
-        // var current_cmd_idx = current_cmd_idxs[domain_name]; 
-
         var domain_name = this.value
         var current_content = $('#content_' + current_domain);
         current_content.hide();
 
         var desired_content = $('#content_' + domain_name);
         desired_content.show();
-
-        var vid = $("#vid_" + domain_name);
-        var start_time = vid_start_times[domain_name];
-        var end_time = vid_end_times[domain_name];
-        playSeg(vid, start_time, end_time, domain_name);
 
         // set current to desired
         current_domain = domain_name;
