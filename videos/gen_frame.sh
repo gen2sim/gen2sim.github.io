b6cb35e384ca9ec1675ceacec8c0817dc6538f51
Pushkal Katara <pushkalkatara@pushkals-air.wifi.local.cmu.edu>
2023-12-21 15:41:00 -0500

    minor

--- videos/gen_frame.sh
+++ videos/gen_frame.sh
@@ -1,5 +1,5 @@
-input_folder=videos/part_structures/left_door_cabinet
-output_folder=videos/part_structures/left_door_cabinet
+input_folder=videos/part_structure
+output_folder=videos/part_structure
 # Iterate over each MP4 file in the input folder
 for file in "$input_folder"/*.mp4; do
     # Extract the filename without extension
@@ -13,3 +13,21 @@ for file in "$input_folder"/*.mp4; do
 
     echo "Extracted the first frame of $file"
 done
+
+
+# input_folder=/Users/pushkalkatara/Desktop/Gen2Sim/videos_oct26
+# output_folder=/Users/pushkalkatara/website/eureka-research.github.io/videos/part_structures
+# # Iterate over each MP4 file in the input folder
+# for file in "$input_folder"/*.mp4; do
+#     # Extract the filename without extension
+#     filename=$(basename "$file")
+#     filename="${filename%.*}"
+
+#     # Use ffmpeg to extract the last frame and save it as PNG
+#     # ffmpeg -i "$file" -vf "select=gte(n\, $(ffprobe -v error -count_frames -select_streams v:0 -show_entries stream=nb_frames -of csv=p=0 "$file"))" -vframes 1 "$output_folder/$filename.png"
+#     # ffmpeg -sseof -3 -i "$file" -update 1 -q:v 1 "$output_folder/$filename.png"
+
+#     ffmpeg -i "$file" -c:v libx264 "$output_folder/$filename.mp4"
+
+#     echo "Extracted the first frame of $file"
+# done
